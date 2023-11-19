20e847aefab85c72d90a99fe792cf0e39cd7de8a
Nayan chandrakar <chandrakarnayan96@gmail.com>
2023-11-19 09:02:55 +0530

    project setup

---
+++ project.tsx
@@ -0,0 +1,6 @@
+// npx create-next-app@latest netflix --typescript --tailwind --eslint
+// npx shadcn-ui@latest init 
+// npx shadcn-ui@latest add form
+// npx shadcn-ui@latest add Input 
+// npx shadcn-ui@latest add button
+// npm i axios 
